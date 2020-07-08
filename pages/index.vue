747d995afe23c07e88b404a9ccad39c5e86e0428
James King <james@wearecrystal.uk>
2020-07-08 17:53:00 +0100

    initial index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+  computed: mapState(['now']),
+};
 </script>
 
 <template>
-  <div>
-    <h1>movements</h1>
+  <div class="container p-20">
+    <h1 class="mb-2 uppercase">movements</h1>
+    <div>{{ now }}</div>
   </div>
 </template>
 
