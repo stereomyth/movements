d622277b6ffea8740f0cb27b4ee7c0870aca2465
James King <james@wearecrystal.uk>
2020-07-08 16:39:34 +0100

    static nuxt tailwind init

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+export default {
+  mode: "spa",
+  target: "static",
+
+  head: {
+    title: "Collected Movements",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "REPLACE_ME" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  components: true,
+  buildModules: ["@nuxtjs/tailwindcss"],
+
+  build: {}
+};
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <h1>movements</h1>
+  </div>
+</template>
+
+<style></style>
